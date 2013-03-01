28a5e364b150ea9b3b796788e5517f367aa3479f
Morita Yasuaki <zousandazou@gmail.com>
2013-03-01 18:14:05 +0900

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,18 +2,18 @@ import cv2
 import sys
 import numpy as np
 from scipy import special
-
-
+from scipy import misc
+from os import path
 def preprocess(img):
   print "prepocessed.."
   dimg = downSample(img)
   inpaints, mask = reflectionMask(dimg)
-  clbpos = calibration(inpaints)
-  dimg[:,clbpos]=np.array([0,0,0])
-  cv2.imshow("dimg",dimg)
-  cv2.imshow("mask",mask)
-  cv2.imshow("inpaints",inpaints)
-  cv2.waitKey(0)
+  (cx,cy,d) = calibration(inpaints)
+  dimg2 = inpaints[cy-d:cy+d,cx-d:cx+d]
+  dimg2 = cv2.resize(dimg2,(256,256))
+  # cv2.imshow("mask",mask)
+  # cv2.imshow("inpaints",inpaints)
+  return dimg2
 
 def objSample(img):
   electrode(ccimg)
@@ -27,11 +27,17 @@ def downSample(img):
   return cv2.resize(img,(w/4,h/4))
 
 def reflectionMask(img):
-  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-  ret, mask = cv2.threshold(gray, 0.75, 1, cv2.THRESH_BINARY)
-  uimg = (256*img).astype(np.uint8)
-  img = cv2.inpaint(uimg, mask.astype(np.uint8), 10, cv2.INPAINT_TELEA)
-  img.astype(np.float32)/256
+  lab = cv2.cvtColor(img,cv2.COLOR_BGR2LAB)
+  l = lab[:,:,0]
+  a = lab[:,:,1]
+  b = lab[:,:,2]
+  ul = (l*2.56).astype(np.uint8)
+  ule = cv2.equalizeHist(ul)
+  ret, mask = cv2.threshold(ule, 245, 1, cv2.THRESH_BINARY)
+  iul = cv2.inpaint(ul, mask.astype(np.uint8), 10, cv2.INPAINT_TELEA)
+  ifl = (iul.astype(np.float32))/2.56
+  lab = np.array([ifl,a,b]).swapaxes(0,1).swapaxes(1,2)
+  img = cv2.cvtColor(lab,cv2.COLOR_LAB2BGR)
   return (img,mask)
 
 
@@ -43,10 +49,17 @@ def calibration(img):
   match = cv2.matchTemplate(gray,mirror,method=cv2.TM_CCOEFF_NORMED)
   d = np.argmax(match)
   centerx = (w+d-s)/2
-  centery = 
-  return center
+  gray = cv2.equalizeHist((gray[h/4:h*3/4,centerx-w*2/5:centerx+w*2/5]*256).astype(np.uint8))
+  m = cv2.moments(gray)
+  centery = m['m01']/m['m00'] + h/4
+  vary = m['mu20']
+  varx = m['mu02']
+  sd = np.sqrt(varx)
+  print (centerx,centery,sd*0.0012)
+  return (centerx,centery,sd*0.0012)
 
-pname, fname = sys.argv
+pname, outputdir, fname = sys.argv
+bname = path.basename(fname)
 print "load " + fname + " ..."
 img = cv2.imread(fname).astype(np.float32)/256.0
-preprocess(img)
+cv2.imwrite(outputdir + "/" + bname, preprocess(img)*256)
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sys
 import scipy
 from scipy import misc, special
 from scipy.ndimage import filters
-
+import numpy.linalg
 def sigmoid(x):
   return 1/(1+exp(-x))
 
@@ -18,13 +18,77 @@ def nsigmoid(x):
 def normalize(x):
   return 0.5*(1+special.erf((x-x.mean())/x.std()/2))
 
+class PCA(object):
+  def __init__(self, dim):
+    self.dim = dim
+
+  def calc(self, src):
+    shape = src.shape
+    mu = (sum(numpy.asarray(src,dtype=numpy.float32)) /float(len(src)))
+    src_m = (src - mu).T
+    if (shape[0] < shape[1]):
+      if (shape[0] < self.dim): self.dim = shape[0]
+      
+      print>>sys.stderr,"convariance matrix..."
+      n = numpy.dot(src_m.T, src_m)/float(shape[0])
+      print>>sys.stderr,"done"
+
+      print>>sys.stderr,"eigen value decomposition"
+      l,v = numpy.linalg.eig(n)
+      idx = l.argsort()
+      l = l[idx][::-1]
+      v = v[:,idx][:,::-1]
+      print>>sys.stderr,"done"
+
+      vm=numpy.dot(src_m, v)
+      for i in range(len(l)):
+        if l[i]<=0:
+          v = v[:,:i]
+          l = l[:i]
+          if (self.dim < i): 
+            self.dim = i
+          break
+        vm[:,i]=vm[:,i]/numpy.sqrt(shape[0]*l[i])
+    else:
+      if shape[1] < self.dim:
+        self.dim = shape[1]
+      cov = numpy.dot(src_m, src_m.T)/float(shape[0])
+      l, vm = numpy.linalg.eig(cov)
+      idx = l.argsort()
+      l = l[idx][::-1]
+      vm = vm[:,idx][:,::-1]
+
+    self.eigenvectors = vm[:,:self.dim]
+    self.eigenvalues = l[:self.dim]
+    self.mean = mu
+  def project(self,vec):
+    return numpy.dot(self.eigenvectors.T,(vec-self.mean).T).T
+
+  def backproject(self,vec):
+    return (numpy.dot(self.eigenvalues,vec.T)).T+self.mean
 
 fnames = sys.argv[1:]
 imgs=[]
 for fname in fnames:
-  bgrimg = cv2.imread(fname).astype(float32)/256.0
-  (h,w,d) = bgrimg.shape
-  bgrimg = cv2.resize(GaussianBlur(bgrimg,(5,5),1.4),(w/4,h/4))
-  imgs.append(bgrimg)
+  imgs.append(cv2.imread(fname).astype(numpy.float32))
 
+data=[]
+for img in imgs:
+  data.append(numpy.reshape(img,256*256*3))
 
+data = numpy.asarray(data)
+print data.shape
+pca = PCA(40)
+pca.calc(data)
+print pca.eigenvectors.shape
+print pca.eigenvalues.shape
+p1 = pca.project(data)
+print p1.shape
+v1 = pca.backproject(p1)
+print v1.shape
+# d,b = v1.shape
+# for i in range(d):
+  # img = v1[i,:,:,:]
+  # img = numpy.reshape(img,(256,256,3))
+  # cv2.imshow("test",img.astype(uint8))
+  # cv2.waitKey(0)
