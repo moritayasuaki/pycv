3ac9ceb071829577d89f438c297f58adbc87a40d
MoritaYasuaki <zousandazou@gmail.com>
2013-03-13 08:35:39 +0900

    cent

--- centerize.py
+++ centerize.py
@@ -30,14 +30,16 @@ def rot180(gray):
 def psymPoint(im):
   """
   >>> y,x = psymPoint(testim)
-  >>> rr,cc = draw.circle(y,x,5)
+  >>> rr,cc = draw.circle(y,x,10)
   >>> testim[rr,cc] = 0
   >>> imshow(testim)
   >>> show()
   """
   l,a,b = color.rgb2lab(im.astype(np.float32)/256).transpose(2,0,1)
   window = hamming2d(l.shape)
-  img = b * window
+  l = ndi.filters.gaussian_filter(l,8,mode='wrap')
+  img = l * window # + ndi.filters.gaussian_filter(l,16,mode='wrap') * (1 - window)
+  show()
   rimg = rot180(img)
   fimg = fft2(img)
   frimg = fft2(rimg)
@@ -45,9 +47,9 @@ def psymPoint(im):
   fcorr /= np.abs(fcorr)
   dy, dx = ndi.measurements.maximum_position( np.abs(ifft2(fcorr)) )
   if (dy > l.shape[0]/2):
-    dy = l.shape[0]/2 - dy
+    dy -= l.shape[0]
   if (dx > l.shape[1]/2):
-    dx = l.shape[1]/2 - dx
+    dx -= l.shape[1]
   dy /= 2
   dx /= 2
   return l.shape[0]/2+dy, l.shape[1]/2+dx
